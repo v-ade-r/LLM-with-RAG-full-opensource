e281ffa289501e322f03e6e726008576bd391d73
Krzysztof Zakowicz <157993314+v-ade-r@users.noreply.github.com>
2024-05-13 01:48:35 +0200

    Update LLM_RAG_Flask.py

--- LLM_RAG_Flask.py
+++ LLM_RAG_Flask.py
@@ -6,8 +6,10 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from flask import Flask, request, render_template
 import os
 
@@ -42,13 +44,31 @@ def RAG_test():
         pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf.filename)
         pdf.save(pdf_path)
         loader = PyPDFLoader(pdf_path)
-        docs_split = loader.load_and_split()
+        # docs_split = loader.load_and_split()
+
+        # # Recursive Splitter - 1000/200 good for finding detailed info, but then almost as slow as Semantic.
+        # # For more general questions, larger chunk_size needed
+        # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        # docs_split = loader.load_and_split(text_splitter)
+
+        # Semantic Chunking - painfully slow (it took more than 12min for 100 pages PDF), but usually very accurate
+        docs_split = loader.load()
+        hf_embeddings = HuggingFaceEmbeddings()
+        text_splitter = SemanticChunker(hf_embeddings, breakpoint_threshold_type="percentile")
+        docs_split = text_splitter.split_documents(docs_split)
 
     elif urls:
         url_list = urls.split('\n')
         docs = [WebBaseLoader(url).load() for url in url_list]
         docs_list = [item for sublist in docs for item in sublist]
-        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+        
+        # #Character Chunking - fast and usually good enough
+        # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+        # docs_split = text_splitter.split_documents(docs_list)
+
+        # Semantic Chunking - for most articles, fast enough, I believe the quality justifies time consumption
+        hf_embeddings = HuggingFaceEmbeddings()
+        text_splitter = SemanticChunker(hf_embeddings, breakpoint_threshold_type="percentile")
         docs_split = text_splitter.split_documents(docs_list)
 
     vectorstore = Chroma.from_documents(
